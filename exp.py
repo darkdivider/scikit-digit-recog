56dc28ade8f802d28645284281c3843d6a844519
user-unix <nair.2@iitj.ac.in>
2023-09-09 11:23:05 +0530

    hyperparameters tuning

--- exp.py
+++ exp.py
@@ -24,7 +24,20 @@ import utils
 # download dataset
 digits = datasets.load_digits()
 
-# get trained model
-# the predict_and_eval is incorporated in the split_train.. in utils.
-model = utils.split_train_dev_test(digits.images, digits.target, )
+# combination sof test sizes and dev sizes
+test_sizes =[0.1, 0.2, 0.3];
+dev_sizes = [0.1,0.2,0.3];
+test_dev_combs=sum([[(test_size, dev_size)for test_size in test_sizes]for dev_size in dev_sizes],[])
 
+# combination of hyperparamters
+Cs=[0.25, 0.5, 1, 2, 4, 8, 16]
+gammas = [0.00025, 0.0005, 0.001, 0.002, 0.004, 0.008, 0.0016]
+h_param_combs = utils.gen_hparams(Cs, gammas)
+
+# hyperparameter tuning
+for test_size, dev_size in test_dev_combs:
+    print(f'test_size: {test_size:0.3f}, dev_size={dev_size:0.3f}, train_size:{1-dev_size-test_size:0.3f}')
+    X_train, X_dev, X_test, y_train, y_dev, y_test = utils.train_dev_test_split(digits.images, digits.target, test_size, dev_size)
+    model, best_h_params = utils.tune_params(X_train, y_train, X_dev, y_dev, h_param_combs)
+    print(f'test_acc: {utils.check_acc(model, X_test, y_test):0.3f}')
+    print('----------------------------------------------------')
