9050b21d5e8a1b6f8628d58f2b41f7a678a750f5
user-unix <nair.2@iitj.ac.in>
2023-10-07 13:42:36 +0530

    incomplete test sample added

--- test_sample.py
+++ test_sample.py
@@ -6,4 +6,20 @@ def test_hparam_combinations():
     h_param_combs = utils.gen_hparams(Cs, gammas)
     assert (1,0.001) in h_param_combs
     assert (0.5,0.002) in h_param_combs
-    
+
+def test_data-splitting():
+    X,y = datasets.load_digits()
+    X=X[:100,:,:]
+
+def test_model_saving():
+    pass
+
+def create_dummy_data():
+    pass
+
+def tune_params(X_train, y_train, X_dev, y_dev, h_params_combinations):
+    best_accuracy=-1
+    best_model_path=''
+    for h_params in h_params_combinations:
+        pass
+
