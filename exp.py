0ce632e6537076560b745d6176dc4de9774a847f
user-unix <nair.2@iitj.ac.in>
2023-10-21 14:01:11 +0530

    quiz2 part completed

--- exp.py
+++ exp.py
@@ -27,19 +27,26 @@ print(f'Number of total samples in dataset: {digits.target.__len__()}');
 print(f'size of images: {digits.images[0].shape}')
 
 # combination sof test sizes and dev sizes
-test_sizes =[0.1, 0.2, 0.3];
-dev_sizes = [0.1,0.2,0.3];
+test_sizes =[0.2];
+dev_sizes = [0.2];
 test_dev_combs=sum([[(test_size, dev_size)for test_size in test_sizes]for dev_size in dev_sizes],[])
 
-# combination of hyperparamters
-Cs=[0.25, 0.5, 1, 2, 4, 8, 16]
-gammas = [0.00025, 0.0005, 0.001, 0.002, 0.004, 0.008, 0.0016]
-h_param_combs = utils.gen_hparams(Cs, gammas)
+h_param_combs={}
+# combination of hyperparamters for svm
+Cs=[1, 2, 4, 8]
+gammas = [0.00025, 0.0005, 0.001, 0.002]
+h_param_combs['svm'] = utils.gen_hparams([Cs, gammas])
+
+# combination of hyperparamters for decision tree
+criteria=['gini', 'entropy','log_loss']
+max_depths = [2,4,6,8,16,32,64,128]
+h_param_combs['tree'] = utils.gen_hparams([criteria, max_depths])
 
 # hyperparameter tuning
 for test_size, dev_size in test_dev_combs:
-    print(f'test_size: {test_size:0.3f}, dev_size={dev_size:0.3f}, train_size:{1-dev_size-test_size:0.3f}')
-    X_train, X_dev, X_test, y_train, y_dev, y_test = utils.train_dev_test_split(digits.images, digits.target, test_size, dev_size)
-    model, best_h_params = utils.tune_params(X_train, y_train, X_dev, y_dev, h_param_combs)
-    print(f'test_acc: {utils.check_acc(model, X_test, y_test):0.3f}')
-    print('----------------------------------------------------')
+    print(f'test_size: {test_size:0.3f}, dev_size={dev_size:0.3f}, train_size:{1-dev_size-test_size:0.3f}') 
+    clf_prod = utils.tune_params(digits.images, digits.target, test_size, dev_size, h_param_combs['svm'], shuffle=True)
+    clf_new = utils.tune_params(digits.images, digits.target, test_size, dev_size, h_param_combs['tree'], 
+                              'tree', ['criterion','max_depth'],True)
+    # import pdb;pdb.set_trace();
+
